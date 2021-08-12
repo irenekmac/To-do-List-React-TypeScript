3c80f2c507a762841131213f6aa72579c221ff19
irene macapinlac <irenekaymac@gmail.com>
2021-08-12 22:02:38 +1000

    imported typescript flag and created functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react'; //FC import typeScript flag
 import './App.css';
 
-function App() {
+// 'FC' = functional component
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+      <div className="header">
+      <input type="text" placeholder="Task..." />
+      <input type="number" placeholder="Deadline (days)" />
+      </div>
+      <div className="todoList"></div>
+      </div>);
+
 }
 
 export default App;

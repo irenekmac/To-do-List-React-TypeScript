b8888f8587acf63c8ca7086a6091029b2d112dae
irene macapinlac <irenekaymac@gmail.com>
2021-08-13 23:35:41 +1000

    added button and css styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,11 @@ const App: FC = () => {
   return (
       <div className="App">
       <div className="header">
-      <input type="text" placeholder="Task..." />
-      <input type="number" placeholder="Deadline (days)" />
+      <div className="inputContainer">
+        <input type="text" placeholder="Task..." />
+        <input type="number" placeholder="Deadline (days)" />
+      </div>
+      <button>Add Task</button>
       </div>
       <div className="todoList"></div>
       </div>);
